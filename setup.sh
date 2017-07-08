51d6807f8af05267e9a87bf2c3d3edeb4f50d740
bo-ko <cgxpnwqb@126.com>
2017-07-09 01:47:31 +0800

    setup.sh-update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
+sleep 3
 if which apt-get >/dev/null; then
 sudo apt-get install -y vim exuberant-ctags vim-scripts  xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
@@ -16,7 +17,7 @@ mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "boko-vim正在努力为您安装bundle程序" > boko-vim
+echo "boko-vim正在努力为您安装插件" > boko-vim
 echo "安装完毕将自动退出" >> boko-vim
 echo "请耐心等待" >> boko-vim
 vim boko-vim -c "BundleInstall" -c "q" -c "q"
